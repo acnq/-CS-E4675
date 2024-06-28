64fd41dee89d5ebf35599acd12106184020d9f01
acnq <fanyuanhaotc@163.com>
2024-06-29 01:29:22 +0300

    finish 5.11

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import BlogFrom from './components/BlogForm' 
 
 //"username": root,
 //"password": sekret
@@ -13,18 +15,16 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-
-  // blog related
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+  const [update, setUpdate] = useState(null)
 
   // useEffects
   useEffect(() => {
-    blogService.getAll().then(blogs =>
+    blogService.getAll().then(blogs => {
       setBlogs( blogs )
-    )  
-  }, [])
+      console.log(update)
+    })  
+  }, [update]) //used for keep user visibe
+  // not know exactly why.
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
     if (loggedUserJSON) {
@@ -35,7 +35,7 @@ const App = () => {
   }, [])
 
 
-  // helper functions
+  // helper functions_p1: about login
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -66,16 +66,24 @@ const App = () => {
       }, 5000)
     }
   }
-  
-  const addBlog = (event) => {
-    event.preventDefault()
+  const logout = () => {
+    setUser(null)
+    blogService.setToken('')
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUsername('')
+    setPassword('')
+  }
+
+  // helper functions_p2: about blogs
+  const createBlog = (blogObject, setTitle, setAuthor, setUrl) => {
     blogService
-      .create({ title, author, url })
+      .create(blogObject)
       .then(returnedBlog => {
         setErrorMessage(null)
         setMessage(
           `a new blog ${returnedBlog.title}`
         )
+        console.log(message)
         setTimeout(() => {
           setMessage(null)
         }, 5000)
@@ -87,18 +95,34 @@ const App = () => {
       .catch(error => {
         setMessage(null)
         setErrorMessage(error.response.data.error)
+        console.log(error.response.data.error)
         setTimeout(() => {
           setErrorMessage(null)
         }, 5000)
       })
-  }
-
-  const logout = () => {
-    setUser(null)
-    blogService.setToken('')
-    window.localStorage.removeItem('loggedBlogappUser')
-    setUsername('')
-    setPassword('')
+  } 
+  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
+  const remove = async (id) => {
+    const blog = blogs.find(n => n.id === id)
+    console.log("id:", id, "blog: ", blog)
+    if (window.confirm(`remove blog ${blog.title} by  ${blog.author}`)) {
+      console.log(user)
+      blogService.setToken(user.token)
+      console.log('user token: ', user.token)
+      console.log('blog.id: ', blog.id)
+      try {
+        await blogService.remove(blog.id, user.token)
+        setUpdate(Math.floor(Math.random() * 100))
+      } catch (error) {
+        setMessage(null)
+        setErrorMessage(error.response.data.error)
+        console.log(error.response.data.error)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+      
+    }
   }
 
   // pages
@@ -135,38 +159,12 @@ const App = () => {
       <h2>blogs</h2>
       <Notification message={message} errorMessage={errorMessage} />
       <p>{user.name} logged in<button onClick={logout}>logout</button></p>
-      <form onSubmit={addBlog}>
-        <div>
-          title:
-          <input
-            type='text'
-            value={title}
-            name='Title'
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            type='text'
-            value={author}
-            name='Author'
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url:
-          <input
-            type='text'
-            value={url}
-            name='Url'
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <button id='create' type='submit'>create</button>
-      </form>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <h2>create new</h2>
+      <Togglable buttonLabel='create new blog'>
+        <BlogFrom createBlog={createBlog}></BlogFrom>
+      </Togglable>
+      {sortedBlogs.map(blog =>
+        <Blog key={blog.id} blog={blog} setUpdate={setUpdate} user={user.username} remove={() => remove(blog.id)}/>
       )}
     </div>
   )
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, userState } from 'react'
+import blogService from '../services/blogs'
+
+const Blog = ({ blog, user, setUpdate, remove }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideRemove = { display: blog.user.username !== user ? 'none' : '' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const like = async event => {
+    event.preventDefault()
+    const likes = blog.likes + 1
+    const newBlog = { ...blog, likes }
+    await blogService.update(blog.id, newBlog)
+    setUpdate(Math.floor(Math.random() * 100))
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div className='blog'>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility} style={hideWhenVisible}>view</button>
+        <button onClick={toggleVisibility} style={showWhenVisible}>hide</button>
+      </div>
+      <div className='fullblog' style={showWhenVisible}>
+        <a href={`${blog.url}`} >{blog.url}</a> <br/>
+        likes {blog.likes}
+        <button onClick={like}>like</button> <br/>
+        {blog.user?.username} <br/>
+        <button onClick={remove} style={hideRemove}>remove</button>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
