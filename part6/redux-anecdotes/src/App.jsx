d6915606f31f655b862b925d16d45476b1a9ec11
acnq <fanyuanhaotc@163.com>
2024-06-30 14:44:48 +0300

    finish 6.13 have to make Notification to jsx

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 import Filter from './components/Filter'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <h2>Anecdotes</h2>
       <Filter />
+      <Notification />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,29 @@
-const filterReducer = ( state = 'ALL', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload
-    default:
-      return state
-  }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: 'ALL',
+  reducers: {
+    filterChange: (state, action) => action.payload,
+  },
+})
 
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
+// const filterReducer = ( state = 'ALL', action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export const filterChange = filter => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter,
+//   }
+// }
+
+// export default filterReducer
